1aa9bb5a5b3f284ff4146d931d1b121580c5fc82
muragoness <102125857+muragoness@users.noreply.github.com>
2022-04-29 20:25:55 +0900

    カラム名の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,13 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   
-  validates :name, presence: true
+  validates :nickname, presence: true
   validates :email, presence: true
   validates :last_name, presence: true
   validates :first_name, presence: true
   validates :last_name_kana, presence: true
   validates :first_name_kana, presence: true
-  validates :birthday
+  validates :birthday, presence: true
 
 
   has_many :items
