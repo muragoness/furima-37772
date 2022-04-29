dfc734850ca7a2fd1892ea2ba061f65d8a75604b
muragoness <102125857+muragoness@users.noreply.github.com>
2022-04-29 16:21:51 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         
+  
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :last_name_kana, presence: true
+  validates :first_name_kana, presence: true
+  validates :birthday
+
+
+  has_many :items
+  has_many :purchases
+
+
 end
