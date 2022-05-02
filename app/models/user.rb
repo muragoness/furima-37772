235e5d4a5d359ea805eed6c99c4f65696ed01411
muragoness <102125857+muragoness@users.noreply.github.com>
2022-05-02 12:49:59 +0900

    コードレビューの修正2

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,8 @@ class User < ApplicationRecord
   
   validates :nickname, presence: true
   
-  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥々]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥々]+\z/}
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
