db07298893b62543b4d59104f984b57e6c2cf638
muragoness <102125857+muragoness@users.noreply.github.com>
2022-05-02 12:22:52 +0900

    コードレビューの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,18 +6,21 @@ class User < ApplicationRecord
 
   
   validates :nickname, presence: true
-  validates :email, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/}
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :birthday, presence: true
 
 
-  has_many :items
-  has_many :purchases
+  #has_many :items
+  #has_many :purchases
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX, message: 'は半角英数を両方含む必要があります'}
 
   
-  validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, message: "Password include both letters and numbers"
+  # validates_format_of :password, with: /([0-9].*[a-zA-Z]|[a-zA-Z].*[0-9])/, message: "Password include both letters and numbers"
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,90 +1,148 @@
 require 'rails_helper'
-
-
 RSpec.describe User, type: :model do
   before do
     @user = FactoryBot.build(:user)
   end
 
   describe 'ユーザー新規登録' do
-    context '新規登録が上手く行く時' do
-      it "nicknameとemail、passwordとpassword_confiramation,lasty_name,first_name,last_name_kana,first_name_kana,birthdayが存在すれば登録できる" do
-        expect(@user).to be_valid
-      end
-      it "passwordが6文字以上の半角英数字混合であれば登録できる" do
-        @user.password = "000aaa"
-        @user.password_confirmation = "000aaa"
-        expect(@user).to be_valid
-      end
-      it "last_nameが全角文字であれば登録できる" do
-        @user.last_name = "山田"
-        expect(@user).to be_valid
-      end
-      it "first_nameが全角文字であれば登録できる" do
-        @user.first_name = "太郎"
-        expect(@user).to be_valid
-      end
-      it "last_name_kanaが全角カナであれば登録できる" do
-        @user.last_name_kana = "ヤマダ"
-        expect(@user).to be_valid
-      end
-      it "first_name_kanaが全角カナであれば登録できる" do
-        @user.first_name_kana = "タロウ"
+    context '新規登録がうまくいく時' do
+      it '新規登録できる' do
         expect(@user).to be_valid
       end
     end
+    
+    context '新規登録がうまくいかない時' do
 
-    context '新規登録が上手くいかない時' do
-      it "nicknameが空だと登録できない" do
-        @user.nickname = ""
+
+      it 'nicknameが空だと登録できない' do
+    
+        @user.nickname = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Nickname can't be blank"
+        expect(@user.errors.full_messages).to include("Nickname can't be blank")
       end
-      it "emailが空だと登録できない" do
-        @user.email = ""
+      it 'emailが空では登録できない' do
+        @user.email = ''
         @user.valid?
-        expect(@user.errors.full_messages).to include "Email can't be blank"
+        expect(@user.errors.full_messages).to include("Email can't be blank")
       end
-      it "emailに一意性がないと登録できない" do
+      it 'emailに一意性がないと登録できない' do
         @user.save
         another_user = FactoryBot.build(:user, email: @user.email)
         another_user.valid?
         expect(another_user.errors.full_messages).to include('Email has already been taken')
       end
-      it "passwordが空では登録できない" do
-        @user.password = ""
+      it 'メールアドレスは@を含まないと登録できない' do
+        @user.email = 'example.com'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Email is invalid')
+      end
+      it 'passwordが空だと登録できない' do
+        @user.password = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Password can't be blank")
       end
-      it "passwordが5文字以下であれば登録できない" do
-        @user.password = "00aaa"
-        @user.password_confirmation = "000aaa"
+      it 'passwordが5文字以下であれば登録できない' do
+        @user.password = '123qw'
+        @user.password_confirmation = '123qw'
         @user.valid?
-        expect(@user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+       
+        expect(@user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
       end
-      
-      
-      it "passwordが存在してもpassword_confirmationが空では登録できない" do
-        @user.password = "123qwe"
-        @user.password_confirmation = ""
+
+
+      it 'passwordは半角数字だけでは登録できない' do
+        @user.password = '111111'
+        @user.password_confirmation = '111111'
+        @user.valid?
+        
+        expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
+      end
+
+
+      it 'passwordは半角英字だけでは登録できない' do
+        @user.password = 'aaaaaa'
+        @user.password_confirmation = 'aaaaaa'
+        @user.valid?
+        
+        expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
+      end
+
+      it 'passwordは半角英数字でないと登録できない' do
+        @user.password = '123ｑｗｅ'
+        @user.password_confirmation = '123ｑｗｅ'
+        @user.valid?
+        
+        expect(@user.errors.full_messages).to include('Password は半角英数を両方含む必要があります')
+      end
+     
+
+
+
+
+      it 'passwordとpassword_confirmationが不一致では登録できない' do
+        @user.password = '123456'
+        @user.password_confirmation = '1234567'
         @user.valid?
         expect(@user.errors.full_messages).to include("Password confirmation doesn't match Password")
       end
-      it "生年月日がない場合は登録できない" do
+      it 'last_nameがない場合は登録できない' do
+        @user.last_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name can't be blank", 'Last name is invalid')
+      end
+
+      it 'first_nameがない場合は登録できない' do
+        @user.first_name = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name can't be blank", 'First name is invalid')
+      end
+      it 'Last_nameが全角入力でなければ登録できない' do
+        @user.last_name = 'wwwww'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name is invalid')
+      end
+      it 'first_nameが全角入力でなければ登録できない' do
+        @user.first_name = 'wwwww'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      it 'last_nameは全角（漢字・ひらがな・カタカナ）での入力でなければ登録できない' do
+        @user.last_name = "\bＡＡＡＡＡ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('Last name is invalid')
+      end
+      it 'first_nameは全角（漢字・ひらがな・カタカナ）での入力でなければ登録できない' do
+        @user.first_name = "\bＡＡＡＡＡ"
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name is invalid')
+      end
+      it 'last_name_kanaがない場合は登録できないこと' do
+        @user.last_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Last name kana can't be blank", 'Last name kana is invalid')
+      end
+      it 'first_name_kanaがない場合は登録できないこと' do
+        @user.first_name_kana = ''
+        @user.valid?
+        expect(@user.errors.full_messages).to include("First name kana can't be blank", 'First name kana is invalid')
+      end
+      it 'last_name_kanaが全角(カタカナ)入力でなければ登録できない' do
+        @user.last_name_kana = 'ｱｲｳｴｵ'
+        @user.valid? 
+        expect(@user.errors.full_messages).to include('Last name kana is invalid')
+      end
+      it 'first_name_kanaが全角(カタカナ)入力でなければ登録できない' do
+        @user.first_name_kana = 'ｱｲｳｴｵ'
+        @user.valid?
+        expect(@user.errors.full_messages).to include('First name kana is invalid')
+      end
+      it '生年月日がない場合は登録できない' do
         @user.birthday = ''
         @user.valid?
         expect(@user.errors.full_messages).to include("Birthday can't be blank")
       end
-      it "passwordは半角数字だけでは登録できない" do
-        @user.password = '111111'
-        @user.password_confirmation = '111111'
-        @user.valid?
-      
-        expect(@user.errors.full_messages).to include("Password Password include both letters and numbers")
-      end
-      
-      
+    
+
     end
   end
 end
-
