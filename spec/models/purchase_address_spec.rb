0bedd2f554b67fa87a0e5bdd41ae50f20365cb41
muragoness <102125857+muragoness@users.noreply.github.com>
2022-05-28 13:20:43 +0900

    購入画面エラー文の日本語化

--- spec/models/purchase_address_spec.rb
+++ spec/models/purchase_address_spec.rb
@@ -26,61 +26,74 @@ RSpec.describe PurchaseAddress, type: :model do
       it 'token(クレジットカード情報)が空だと購入ができない' do
         @purchase_address.token = nil
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Token can't be blank")
+      
+        expect(@purchase_address.errors.added?(:token, :blank))
       end
 
       it '郵便番号が空だと購入ができない' do
         @purchase_address.postal_code = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.messages).to include("Postal code can't be blank", 'is invalid')
+       
+        expect(@purchase_address.errors.added?(:postal_code, :blank))
+        expect(@purchase_address.errors.added?(:postal_code, :invalid))
       end
       it '郵便番号にハイフンがないと登録できない' do
         @purchase_address.postal_code = '12345678'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include('Postal code is invalid')
+        
+        expect(@purchase_address.errors.added?(:postal_code, :invalid))
       end
       it 'Prefectures_idが空だと購入できない' do
         @purchase_address.prefectures_id = nil
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Prefectures can't be blank")
+      
+        expect(@purchase_address.errors.added?(:prefectures_id, :invalid))
       end
       it '都道府県が選択されていないと購入できない' do
         @purchase_address.prefectures_id = 1
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include('Prefectures must be other than 1')
+        
+        expect(@purchase_address.errors.added?(:prefectures_id, :mandatory))
       end
       it 'municipalitiesが空だと購入できない' do
         @purchase_address.municipalities = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Municipalities can't be blank")
+      
+        expect(@purchase_address.errors.added?(:municipalities, :blank))
       end
 
       it 'addressが空だと購入できない' do
         @purchase_address.address = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Address can't be blank")
+      
+        expect(@purchase_address.errors.added?(:purchase_address, :blank))
       end
 
       it 'phone_numberが空だと購入できない' do
         @purchase_address.phone_number = ''
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include("Phone number can't be blank")
+  
+        expect(@purchase_address.errors.added?(:phone_number, :blank))
       end
       it '電話番号は数字のみでないと（ハイフンが含まれていると）購入できない' do
         @purchase_address.phone_number = '123-4567891'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include('Phone number is invalid')
+       
+        expect(@purchase_address.errors.added?(:phone_number, :invalid))
       end
       it '電話番号が12桁以上では購入できない' do
         @purchase_address.phone_number = '080123456789'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include('Phone number is invalid')
+      
+        expect(@purchase_address.errors.added?(:phone_number, :invalid))
+
       end
 
       it '電話番号が9桁以下では購入できない' do
         @purchase_address.phone_number = '080123456'
         @purchase_address.valid?
-        expect(@purchase_address.errors.full_messages).to include('Phone number is invalid')
+     
+        expect(@purchase_address.errors.added?(:phone_number, :invalid))
       end
 
     end
